90d49503aae2d010bb79137219e8df2248b7ed1d
alexander <alexander.satizabal1007@gmail.com>
2019-01-16 22:15:44 -0500

    implementadas alertas de pago en contratos

--- server/controllers/contratos.controller.js
+++ server/controllers/contratos.controller.js
@@ -9,6 +9,13 @@ async function validar_numerCon(numero_contr) {
     return await Contrato.find({ ct_number: numero_contr, state: 'true' });
 }
 
+async function definirct_number(tipo_cont) {
+    return await Contrato.find({ tipo_contrato: tipo_cont, state: true }).sort({ $natural: -1 }).limit(1);
+}
+
+async function definirct_identi() {
+    return await Contrato.find({ state: true }).sort({ $natural: -1 }).limit(1);
+}
 
 //listar contratos
 contratoCtrl.getContratos = async (req, res) => {
@@ -20,23 +27,170 @@ contratoCtrl.getContratos = async (req, res) => {
     // .catch(err => console.log(err));
 };
 
-async function definirct_number(tipo_cont) {
 
-    return await Contrato.find({ tipo_contrato: tipo_cont, state: true }).sort({ $natural: -1 }).limit(1);
+// crear contrato
+contratoCtrl.createContrato = async (req, res) => {
 
-}
+    const Valid = await definirct_number(req.params.tipo_contrato);
+    var tip_con = req.params.tipo_contrato;
+    var nume_cont;
+
+    if (Valid == 0) {
+
+        if (tip_con = 'ocasional') {
+            nume_cont = '3000';
+        }
+        if (tip_con = 'colegios') {
+            nume_cont = '6000';
+        }
+        if (tip_con = 'empresas') {
+            nume_cont = '9000';
+        }
 
 
-async function definirct_identi() {
-    return await Contrato.find({ state: true }).sort({ $natural: -1 }).limit(1);
+        const contrato = new Contrato({
+            id_cliente: req.body.id_cliente,
+            tipo_contrato: req.body.tipo_contrato,
+            renewable: req.body.renewable,
+            ct_object: req.body.ct_object,
+            pass_number: req.body.pass_number,
+            car_number: req.body.car_number,
+            route: req.body.route,
+            return_route: req.body.return_route,
+            start: req.body.start,
+            end: req.body.end,
+            value: req.body.value,
+            payment: req.body.payment,
+            sign_date: req.body.sign_date,
+            ct_number: nume_cont,
+            estadoContrato: req.body.estadoContrato,
+            fecha_Pago: req.body.fecha_Pago,
+            state: true
 
-}
+        });
+
+        await contrato.save();
+        //invoco funcion ultimo contrato,guardo el numero del contrato
+        const Valid_idenC = await definirct_identi();
+        Valid_idenC.map(async UltimoContrato => {
+            const vehiculos = req.body.vehiculos;
+            const conductores = req.body.conductores;
+            const pasajeros = req.body.pasajeros;
+            const cooperacion = req.body.cooperacion;
+            Permiso.createPermisoCon(UltimoContrato._id, UltimoContrato.car_number, UltimoContrato.start, UltimoContrato.end, vehiculos, conductores, pasajeros, cooperacion);
+        });
 
 
+        //funcion crear permiso que serio Permiso.funcion (que creo en permiso.controller) y debo enviar variable numero contrato y req.body.permisos 
+        //Permiso.createPermiso(Valid_idenC, req.body.Permiso);
+
+        res.json({
+            status: 'Contrato Guardado Exitosamente', success: 'true'
+        });
+    }
+
+    else {
+
+        Valid.map(async dato => {
+
+            nume_cont = (parseInt(dato.ct_number) + 1).toString();
+
+            const contrato = new Contrato({
+                id_cliente: req.body.id_cliente,
+                tipo_contrato: req.body.tipo_contrato,
+                renewable: req.body.renewable,
+                ct_object: req.body.ct_object,
+                pass_number: req.body.pass_number,
+                car_number: req.body.car_number,
+                route: req.body.route,
+                return_route: req.body.return_route,
+                start: req.body.start,
+                end: req.body.end,
+                value: req.body.value,
+                payment: req.body.payment,
+                sign_date: req.body.sign_date,
+                ct_number: nume_cont,
+                estadoContrato: req.body.estadoContrato,
+                fecha_Pago: req.body.fecha,
+                state: true
+
+            });
+
+            await contrato.save();
+            //invoco funcion ultimo contrato,guardo el numero del contrato
+            const Valid_idenC = await definirct_identi();
+
+            Valid_idenC.map(async UltimoContrato => {
+
+                const vehiculos = req.body.vehiculos;
+                const conductores = req.body.conductores;
+                const pasajeros = req.body.pasajeros;
+                const cooperacion = req.body.cooperacion;
+                Permiso.createPermiso(UltimoContrato._id, UltimoContrato.car_number, UltimoContrato.start, UltimoContrato.end, vehiculos, conductores, pasajeros, cooperacion);
+            });
+
+            res.json({
+                status: 'Contrato Guardado Exitosamente', success: 'true'
+            });
+        });
+    }
+};
+
+
+// consultar por un contrato especifico
+contratoCtrl.getContrato = async (req, res) => {
+    const contrato = await Contrato.findById(req.params.id);
+    res.json(contrato);
+};
+
+
+//generar alertas de contratos
 contratoCtrl.getAlertasContratos = async (req, res) => {
     const contrato = await Contrato.find({ state: "true" });
     const alertas = [];
     contrato.map(async dato => {
+
+
+        if (dato.payment == "Anticipo 50%" || dato.payment == "Otro") {
+            var fecha_com = moment(dato.fecha_Pago); // captura fecha de fin del contrato
+            var fecha_Actual = moment().format("YYYY-MM-DD"); // capturas la fecha actual del sistema y le da formato
+            var fecha_diferencia = fecha_com.diff(fecha_Actual, 'days');// calcula la diferencia de fechas y se muestra en dias
+
+            //condicion de fechas entre 1 y 15 dias para alerta tipo 1
+            if (fecha_diferencia >= 1 && fecha_diferencia <= 15) {
+                // objeto alerta tipo 1 -> warning amarilla
+                const objAlerta1 = {
+                    numero_contrato: dato.ct_number,
+                    tipo_contra: dato.tipo_contrato,
+                    objeto_contrato: dato.ct_object,
+                    cliente: dato.id_cliente,
+                    alerta: "Fecha de Pago Establecido para el Contrato",
+                    id: dato._id,
+                    fecha: moment(dato.fecha_Pago).format("YYYY-MM-DD"),
+                    fecha_diferencia: fecha_diferencia,
+                    tipo_alerta: 1
+                }
+                alertas.push(objAlerta1);
+            };
+
+            if (fecha_diferencia <= 0) {
+                //objeto alerta tipo 2 -> warning roja
+                const objAlerta2 = {
+                    numero_contrato: dato.ct_number,
+                    tipo_contra: dato.tipo_contrato,
+                    objeto_contrato: dato.ct_object,
+                    cliente: dato.id_cliente,
+                    alerta: "Fecha de Pago Establecido para el Contrato",
+                    id: dato._id,
+                    fecha: moment(dato.fecha_Pago).format("YYYY-MM-DD"),
+                    fecha_diferencia: fecha_diferencia,
+                    tipo_alerta: 2
+                }
+                alertas.push(objAlerta2);
+            };
+
+
+        }
 
         var fecha_com = moment(dato.end); // captura fecha de fin del contrato
         var fecha_Actual = moment().format("YYYY-MM-DD"); // capturas la fecha actual del sistema y le da formato
@@ -116,7 +270,7 @@ contratoCtrl.getAlertasContratos = async (req, res) => {
         var fecha_diferencia = fecha_com.diff(fecha_Actual, 'days');// calcula la diferencia de fechas y se muestra en dias
         if (fecha_diferencia <= 0) {
             //objeto alerta tipo 2 -> warning roja
-            if (estadoContrato = "En ejecucion") {
+            if (estadoContrato == "En ejecucion") {
                 const objAlerta2 = {
                     numero_contrato: dato.ct_number,
                     tipo_contra: dato.tipo_contrato,
@@ -134,120 +288,6 @@ contratoCtrl.getAlertasContratos = async (req, res) => {
         };
 
     });
-};
-
-// crear contrato
-contratoCtrl.createContrato = async (req, res) => {
-
-    const Valid = await definirct_number(req.params.tipo_contrato);
-    var tip_con = req.params.tipo_contrato;
-    var nume_cont;
-
-    if (Valid == 0) {
-
-        if (tip_con = 'ocasional') {
-            nume_cont = '3000';
-        }
-        if (tip_con = 'colegios') {
-            nume_cont = '6000';
-        }
-        if (tip_con = 'empresas') {
-            nume_cont = '9000';
-        }
-
-
-        const contrato = new Contrato({
-            id_cliente: req.body.id_cliente,
-            tipo_contrato: req.body.tipo_contrato,
-            renewable: req.body.renewable,
-            ct_object: req.body.ct_object,
-            pass_number: req.body.pass_number,
-            car_number: req.body.car_number,
-            route: req.body.route,
-            return_route: req.body.return_route,
-            start: req.body.start,
-            end: req.body.end,
-            value: req.body.value,
-            payment: req.body.payment,
-            sign_date: req.body.sign_date,
-            ct_number: nume_cont,
-            estadoContrato: req.body.estadoContrato,
-            state: true
-
-        });
-
-        await contrato.save();
-        //invoco funcion ultimo contrato,guardo el numero del contrato
-        const Valid_idenC = await definirct_identi();
-        Valid_idenC.map(async UltimoContrato => {
-            const vehiculos = req.body.vehiculos;
-            const conductores = req.body.conductores;
-            const pasajeros = req.body.pasajeros;
-            const cooperacion = req.body.cooperacion;
-            Permiso.createPermisoCon(UltimoContrato._id, UltimoContrato.car_number, UltimoContrato.start, UltimoContrato.end, vehiculos, conductores, pasajeros, cooperacion);
-        });
-
-
-        //funcion crear permiso que serio Permiso.funcion (que creo en permiso.controller) y debo enviar variable numero contrato y req.body.permisos 
-        //Permiso.createPermiso(Valid_idenC, req.body.Permiso);
-
-        res.json({
-            status: 'Contrato Guardado Exitosamente', success: 'true'
-        });
-    }
-
-    else {
-
-        Valid.map(async dato => {
-
-            nume_cont = (parseInt(dato.ct_number) + 1).toString();
-
-            const contrato = new Contrato({
-                id_cliente: req.body.id_cliente,
-                tipo_contrato: req.body.tipo_contrato,
-                renewable: req.body.renewable,
-                ct_object: req.body.ct_object,
-                pass_number: req.body.pass_number,
-                car_number: req.body.car_number,
-                route: req.body.route,
-                return_route: req.body.return_route,
-                start: req.body.start,
-                end: req.body.end,
-                value: req.body.value,
-                payment: req.body.payment,
-                sign_date: req.body.sign_date,
-                ct_number: nume_cont,
-                estadoContrato: req.body.estadoContrato,
-                state: true
-
-            });
-
-            await contrato.save();
-            //invoco funcion ultimo contrato,guardo el numero del contrato
-            const Valid_idenC = await definirct_identi();
-
-            Valid_idenC.map(async UltimoContrato => {
-
-                const vehiculos = req.body.vehiculos;
-                const conductores = req.body.conductores;
-                const pasajeros = req.body.pasajeros;
-                const cooperacion = req.body.cooperacion;
-                Permiso.createPermiso(UltimoContrato._id, UltimoContrato.car_number, UltimoContrato.start, UltimoContrato.end, vehiculos, conductores, pasajeros, cooperacion);
-            });
-
-            res.json({
-                status: 'Contrato Guardado Exitosamente', success: 'true'
-            });
-        });
-    }
-};
-
-
-
-// consultar por un contrato especifico
-contratoCtrl.getContrato = async (req, res) => {
-    const contrato = await Contrato.findById(req.params.id);
-    res.json(contrato);
 };
 
 
@@ -289,6 +329,7 @@ contratoCtrl.updateContrato = async (req, res) => {
                     sign_date: req.body.sign_date,
                     ct_number: nume_cont,
                     estadoContrato: req.body.estadoContrato,
+                    fecha_Pago: req.body.fecha,
                     state: req.body.state
                 }
                 await Contrato.findByIdAndUpdate(id, { $set: newContrato }, { new: true });
